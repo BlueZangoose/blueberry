87492425dd96e387fd464c9abb99c56f40d27ae5
BlueZangoose <113498143+BlueZangoose@users.noreply.github.com>
2022-11-15 22:00:05 +0000

    Revert "Fixed effectiveness text and sound fx on dual types"
    This reverts commit f736ca209871f3728f6e5921f3e54d4a81b5de9f.

--- pokered-master/engine/battle/animation_subfiles/play_applying_attack_sound.asm
+++ pokered-master/engine/battle/animation_subfiles/play_applying_attack_sound.asm
@@ -4,33 +4,19 @@ PlayApplyingAttackSound:
 	call WaitForSoundToFinish
 	ld a, [wDamageMultipliers]
 	and $7f
-;	ret z
-;	cp 10
-;	ld a, $20
-;	ld b, $30
-;	ld c, SFX_DAMAGE
-;	jr z, .playSound
-	cp $7f
-	ret z ; immune
-; super effective
-	ld d, a
-	cp %00000010
-	ld a, $e0
-	ld b, $ff
-	ld c, SFX_SUPER_EFFECTIVE
-;	jr nc, .playSound
-	jr z, .playSound
-; not very effective
-	ld a, d
-	cp %00000001
-	ld a, $50
-	ld b, $1
-	ld c, SFX_NOT_VERY_EFFECTIVE
-	jr nc, playSound
-; neutral
+	ret z
+	cp 10
 	ld a, $20
 	ld b, $30
 	ld c, SFX_DAMAGE
+	jr z, .playSound
+	ld a, $e0
+	ld b, $ff
+	ld c, SFX_SUPER_EFFECTIVE
+	jr nc, .playSound
+	ld a, $50
+	ld b, $1
+	ld c, SFX_NOT_VERY_EFFECTIVE
 .playSound
 	ld [wFrequencyModifier], a
 	ld a, b
--- pokered-master/engine/battle/display_effectiveness.asm
+++ pokered-master/engine/battle/display_effectiveness.asm
@@ -1,13 +1,10 @@
 DisplayEffectiveness:
 	ld a, [wDamageMultipliers]
 	and $7F
-	and a
-	ret z ; neutral
-	cp %00000011
-	ret z ; SE to one type and NVE to the other type
-	and %00000001
+	cp $0A
+	ret z
 	ld hl, SuperEffectiveText
-	jr z, .done
+	jr nc, .done
 	ld hl, NotVeryEffectiveText
 .done
 	jp PrintText
